84bc01151f5fc5e6326789c29a0735cad0a29d7f
fenturechance <fenturechance@gmail.com>
2022-01-16 20:12:19 +0800

    add admin flow

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -15,16 +15,18 @@ class CompaniesController < ApplicationController
 
   def new
     @company = Company.new
+    @isNew = true
   end
 
   def edit
+    @isNew = false
   end
   def create
     @company = Company.new company_params
 
     respond_to do |format|
       if @company.save
-        format.html { redirect_to @company, notice: "company was successfully created." }
+        format.html { redirect_to companies_show_admin_path(@company), notice: "company was successfully created." }
         format.json { render :show, status: :created, location: @company }
       else
         format.html { render :new }
@@ -34,8 +36,8 @@ class CompaniesController < ApplicationController
   end
   def update
     respond_to do |format|
-      if @company.update company_params
-        format.html { redirect_to @company, notice: "Company was successfully updated." }
+      if @company.update company_params_edit
+        format.html { redirect_to companies_show_admin_path(@company), notice: "Company was successfully updated." }
         format.json { render :show, status: :ok, location: @company }
       else
         format.html { render :edit }
@@ -54,6 +56,9 @@ class CompaniesController < ApplicationController
     @company = Company.find params[:id]
   end
   def company_params
-    params.require(:company).permit(:companyName, :companyProfile)
+    params.require(:company).permit(:companyName, :companyProfile, :companyPhilosophy, :mediaReports, :salaryAndBenefits)
+  end
+  def company_params_edit
+    params.require(:company).permit(:companyProfile, :companyPhilosophy, :mediaReports, :salaryAndBenefits)
   end
 end
